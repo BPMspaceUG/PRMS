3dd645aacb40cb493a250f1bd362216d16e83e00
dwu300 <info@wuda.at>
2020-02-18 13:42:44 +0100

    video step 6

--- _state_rules/1.php
+++ _state_rules/1.php
@@ -1,5 +1,5 @@
 <?php
-    $allow = false;
+    $allow = [];
     $msg = "";
 
     //--- Input
@@ -7,19 +7,21 @@
     $prjdesc = $param["row"]["prms_project_description"];
 
     // Check if name is not empty
-    $allow = (!empty($prjname));
-    if (!$allow) $msg .= "Name can not be empty!<br>";
+    $allowThis = (!empty($prjname));
+    if (!$allowThis) $msg .= "Name can not be empty!<br>";
+    $allow[] = $allowThis;
 
     // Check Description
     $minLen = 100;
     if (strlen($prjdesc) > 0) {
-        $allow = (strlen($prjdesc) >= $minLen);
-        if (!$allow) $msg .= "Description must have min. $minLen Chars!<br>";
+        $allowThis = (strlen($prjdesc) >= $minLen);
+        if (!$allowThis) $msg .= "Description must have min. $minLen Chars!<br>";
     }
+    $allow[] = $allowThis;
 
     //--- Output
     $script_result = [
-        "allow_transition" => $allow,
+        "allow_transition" => array_unique($allow)[0] === true && count(array_unique($allow)) === 1,
         "show_message" => strlen($msg) > 0,
         "message" => $msg
     ];
