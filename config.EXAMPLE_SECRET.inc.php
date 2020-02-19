c177a1528e79f85a6d7920831fbbbbf638d05e6d
Daniel Wurzer <daniel.wurzer@bpmspace.org>
2020-02-19 15:25:14 +0100

    db name changed

--- config.EXAMPLE_SECRET.inc.php
+++ config.EXAMPLE_SECRET.inc.php
@@ -1,5 +1,5 @@
 <?php
-    // APMS Generated Project (2020-02-19 15:14:02)
+    // APMS Generated Project (2020-02-19 15:25:01)
     // Version: https://github.com/BPMspaceUG/APMS2/tree/
     // ==================================================
     //-- Database
